fe35a1bbd2c72f63d7e93979a40429a8fc5a9b82
Yooinhak <syu3236@gmail.com>
2022-07-31 20:36:51 +0900

    chapter2

--- next.config.js
+++ next.config.js
@@ -1,7 +1,28 @@
 /** @type {import('next').NextConfig} */
+
+const API_KEY = 'c7ac8776ef8b3d295c8717f93a33b0cb';
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  nextConfig,
+  async redirects() {
+    return [
+      {
+        source: '/old-blog/:path*',
+        destination: '/new-sexy-blog/:path*',
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/movies',
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
